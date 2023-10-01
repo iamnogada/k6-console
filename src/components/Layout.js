ffc53a792eb026ce058546ef269b9d8c02ff6155
nogada <seungil.han@gmail.com>
2023-10-01 14:18:31 +0900

    change layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,43 +5,34 @@ function Layout({ children }) {
   const location = useLocation();
 
   return (
-    <div className="flex h-screen bg-gray-100 mb-10">
-      {/* <!-- Sidebar / Navigation --> */}
-      <div className="bg-white w-64 p-6 hidden sm:block">
-        <div className="text-xl mb-4 text-primary font-bold">
-          <Link to="/">K6 Console</Link>
-        </div>
+    <>
+    <header className="bg-primary text-white py-2">
+        <nav className="container mx-auto">
+            <ul className="flex space-x-4">
+                <li><a href="#" className="hover:text-primary-400">K6 Project</a></li>
+                <li><a href="#" className="hover:text-primary-400">UI Sample</a></li>
+                <li><a href="#" className="hover:text-primary-400">About</a></li>
+            </ul>
+        </nav>
+    </header>
 
-        <ul className="mt-14">
-          <li className="mb-2">
-            <Link to="/runtest" className="text-primary font-semibold hover:text-gray-800">
-              Run Test
-            </Link>
-          </li>
-          <li className="mb-2">
-            <Link to="/scripts" className=" text-primary font-semibold hover:text-gray-800">
-              Scripts
-            </Link>
-          </li>
-        </ul>
-      </div>
+    <div className="container mx-auto mt-4 flex">
+        <aside className="w-1/6 bg-secondary-300 p-4">
+            <ul>
+                <li><a href="#" className="block mb-2 hover:text-secondary-800">Run Test</a></li>
+                <li><a href="#" className="block mb-2 hover:text-secondary-800">Profile</a></li>
+                <li><a href="#" className="block mb-2 hover:text-secondary-800">Settings</a></li>
+                <li><a href="#" className="block hover:text-secondary-800">Logout</a></li>
+            </ul>
+        </aside>
 
-      {/* <!-- Main Content --> */}
-      <div className="flex-1 flex flex-col overflow-hidden">
-        {/* <!-- Top Bar --> */}
-        <header className="bg-white p-6">
-          <div className="flex justify-between items-center">
-            <div className="text-xl font-semibold text-gray-700">{location.pathname}</div>
-            <div className="flex items-center">
-              <div className="text-primary font-bold">KloudBank</div>
+        <main className="w-5/6 bg-white p-4 overflow-y-auto border border-secondary-300">
+            <div className="max-h-96 overflow-y-auto">
+                {children}
             </div>
-          </div>
-        </header>
-
-        {/* <!-- Content Area --> */}
-        <main className="flex-grow p-6 mb-10">{children}</main>
-      </div>
+        </main>
     </div>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,32 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#800080',        // Primary purple
-        'light': '#A94CA9',          // Lighter shade
-        'dark': '#660066',           // Darker shade
-        'pale': '#D8BFD8',           // Pale shade
-        'deep': '#4B0082',           // Deep shade
+        primary: {
+          DEFAULT: '#1e1b4b',
+          '50': '#eef2ff',
+          '100': '#e0e7ff',
+          '200': '#c7d2fe',
+          '300': '#a5b4fc',
+          '400': '#818cf8',
+          '500': '#6366f1',
+          '600': '#4f46e5',
+          '700': '#4338ca',
+          '800': '#3730a3',
+          '900': '#312e81',
+        },
+        secondary: {
+          DEFAULT: '#3b0764',
+          '50': '#f5f3f9',
+          '100': '#ebe8f2',
+          '200': '#d0c5e0',
+          '300': '#b3a0ce',
+          '400': '#8e74b4',
+          '500': '#6b4f99',
+          '600': '#553e7d',
+          '700': '#462f66',
+          '800': '#38244e',
+          '900': '#2f1f3f',
+        }
       },
     },
   },
